4e45c13ff27236bd3a0f5bd7c8e7badb7e510e2c
AIZEKKEN <138197646+AIZEKKEN@users.noreply.github.com>
2025-03-26 12:25:16 +0700

    Add files via upload

--- POS project/admin/index.php
+++ POS project/admin/index.php
@@ -1,68 +1,89 @@
-<?php include("header.php"); ?>
-<!-- Content Header (Page header) -->
-<section class="content-header">
-  <div class="container-fluid">
-    </div><!-- /.container-fluid -->
-  </section>
-  <!-- Main content -->
-  <section class="content">
-    <div class="card card-orange">
-      <div class="card-header ">
-        <h3 class="card-title" style="color:aliceblue;">List order</h3>
-        <div align="right">
-  
-        </div>
-      </div>
-      <br>
-      <div class="card-body">
-        <div class="row">
-          
-          <div class="col-md-12">
-            
-          <?php 
+<!DOCTYPE html>
+<html>
 
-                   $act = (isset($_GET['act']) ? $_GET['act'] : '');
-                    if($act =='view'){
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>POS ECS </title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="" href="mem_img/2.jpg">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="assets/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Kanit:400" rel="stylesheet">
 
-                      include('order_detail.php');
 
-                   
-                      }else{
 
-                  include('list_order.php');
-                    } ?>
-      
-          </div>
-          
-        </div>
-      </div>
-      <div class="card-footer">
-        
-      </div>
-      
+</head>
+
+
+<style>
+  .hold-transition {
+    background-image: url("p_img/winter.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+</style>
+
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <div class="login-logo">
+
     </div>
-    
-    
-    
-    
-  </section>
-  <!-- /.content -->
-  
-  
-  <?php include('footer.php'); ?>
-  <script>
-  $(function () {
-  $(".datatable").DataTable();
-  // $('#example2').DataTable({
-  //   "paging": true,
-  //   "lengthChange": false,
-  //   "searching": false,
-  //   "ordering": true,
-  //   "info": true,
-  // });/   "autoWidth": false,
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="card-body login-card-body">
+        <center><img width="250 px" src="mem_img/2.jpg"><br>
+          <h3><b>ECS Mall</h3> <br>
+          <h4>Please Login</h4>
+        </center><br>
+        <!--  <p class="login-box-msg">เข้าสู่ระบบ</p> -->
 
-  });
-  </script>
-  
+        <form action="chk_login.php" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" name="mem_username" id="mem_username" placeholder="Username">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user text-primary"></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" name="mem_password" id="mem_password" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock text-primary"></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="social-auth-links text-center mb-3">
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
+          </div>
+        </form>
+        <!-- /.social-auth-links -->
+      </div>
+      <!-- /.login-card-body -->
+    </div>
+  </div>
+  <!-- /.login-box -->
 </body>
+
+
+<!-- jQuery -->
+<script src="assets/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="assets/bootstrap.bundle.min.js"></script>
+
+
 </html>
